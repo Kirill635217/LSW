24e066af76b77583da9360737b60a0e4edcb1e1b
maple_syrup <ironman60096@gmail.com>
2021-09-19 14:43:13 +0300

    Added 2d assets

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,10 +7,13 @@ public class Player : CharacterMovement
 {
     // direction to move towards
     private Vector2 moveDir;
+
+    private Animator animator;
     
     // Start is called before the first frame update
     new void Start()
     {
+        animator = GetComponent<Animator>();
         base.Start();
     }
 
@@ -24,7 +27,32 @@ public class Player : CharacterMovement
     {
         moveDir.x = Input.GetAxisRaw("Horizontal");
         moveDir.y = Input.GetAxisRaw("Vertical");
-        
+        Vector2 dir = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir.x = -1;
+            animator.SetInteger("Direction", 3);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            dir.x = 1;
+            animator.SetInteger("Direction", 2);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir.y = 1;
+            animator.SetInteger("Direction", 1);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            dir.y = -1;
+            animator.SetInteger("Direction", 0);
+        }
+
+        dir.Normalize();
+        animator.SetBool("IsMoving", dir.magnitude > 0);
+
         Move(moveDir);
     }
 }
